8b21bc364eff21d8e761b7a3f18811f2f1f1c8f4
chloe-mt-cheng <chloecheng@rocketmail.com>
2023-06-26 22:22:33 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include ("../include/header.php");
+?>
+<div class=container><div class=row><div class=col>
+<?php
+include ("index.html");
+?>
+</div></div></div> -->
+<?php
+include ("../include/footer.php");
+?>
+
